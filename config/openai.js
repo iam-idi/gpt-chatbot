295359a7cf68500fb791349f0cdae7939e30dda0
idi <idriscreate@gmail.com>
2024-01-20 08:01:00 +0100

    made bot remember preious conversations

--- config/openai.js
+++ config/openai.js
@@ -1,11 +1,9 @@
-import { Configuration, OpenAIApi} from "openai";
+import openAI from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI-API-KEY,
+const openai = new openAI({
+    apiKey: process.env.OPENAI_API_KEY,
 })
-
-const openai = new OpenAIApi(configuration);
 
 export default openai; 
--- index.js
+++ index.js
@@ -1,2 +1,59 @@
 import openai from "./config/openai.js";
+import colors from "colors";
+import readlineSync from "readline-sync";
 
+const chatHistory = []; //stores chat history
+
+async function main(){
+
+    console.log(colors.bold.green('Welcome to the chatbot program'));
+    console.log(colors.bold.green('You can now start chatting with the bot.'));
+
+    while (true){
+        const userInput = readlineSync.question(colors.blue('\nYOU: '));
+        console.log(colors.yellow('GPT-BOT: '))
+        let botResponse = ""; //stores bot response
+
+        try {
+
+            const messages = chatHistory.map(([role, content]) => ({
+                role,
+                content,
+            }));
+            
+            //add latest user input to messages
+            messages.push({role: 'user', content: userInput});
+
+            //call the api with user input
+            const response = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: messages,
+                stream: true
+            })
+            //exiting process
+            if(userInput.toLowerCase() == 'exit'){
+                for await(let part of response){
+                    process.stdout.write(part.choices[0].delta.content || '')
+                }
+                return
+            }
+            //logging bot's response
+            for await(let part of response){
+                process.stdout.write(part.choices[0].delta.content || '')
+                //saving bot response
+                botResponse += part.choices[0].delta.content || '';
+            }
+
+            //add user and gpt response to chatHistory
+            chatHistory.push(['user', userInput]);
+            chatHistory.push(['assistant', botResponse]);
+            // "\n"+console.log("messages: " + JSON.stringify(messages));
+
+        } catch (error) {
+            console.error(colors.red(error))
+        }
+    }
+
+}
+
+main();
